4927029b4c8300d2508c5ef9b9758779e5dee833
wangjiezhe <wangjiezhe@gmail.com>
2023-03-31 22:58:52 +0800

    Refactor

---
+++ const.ml
@@ -0,0 +1,10 @@
+let csv_301 = "301.csv"
+let tehaionly = false
+let bgtrans = false
+let nologo = true
+let twiimg = false
+let referer = "https://tenhou.net/"
+
+let user_agent =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like \
+   Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54"
